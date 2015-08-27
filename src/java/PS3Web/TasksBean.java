021806d2fbfcfff64f68b9a93097483cf53f2632
Karl <Karl@192.168.1.22>
2015-08-27 16:51:48 +0200

    First working alpha-version. Not very pretty coding or good commenting, but working code. Couldn´t get "rendered" attribute to work which is why empty tables and related elements are always visible.

--- src/java/PS3Web/TasksBean.java
+++ src/java/PS3Web/TasksBean.java
@@ -10,7 +10,6 @@ import com.sun.rowset.CachedRowSetImpl;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
@@ -24,63 +23,52 @@ import javax.sql.rowset.CachedRowSet;
 @ManagedBean(name="TasksBean")
 @RequestScoped
 public class TasksBean {
-    private int taskId;
+
     private int techId;
+
+    private int taskId;
+
+    private int catId;
+    
     private String status;
-    private int UsedTime;
+
+    private int usedTime;
+
     private String comments;
+    
     CachedRowSet tasks;
+
     DataSource ds;
     
     /**
      * Creates a new instance of StudentBean
      */
-    public TasksBean() {
-        this.tasks = null;
-    }
+    public TasksBean() { this.tasks = null; }
 
-    public int getTaskId() {
-        return taskId;
-    }
-
-    public void setTaskId(int id) {
-        this.taskId = id;
-    }    
+    public int getTechId() { return techId; }
+    public void setTechId(int techId) { this.techId = techId; }
     
-    public int getTechId() {
-        return techId;
-    }
-
-    public void setTechId(int techId) {
-        this.techId = techId;
-    }
-
-    public String getstatus() {
-        return status;
-    }
-
-    public void setstatus(String status) {
-        this.status = status;
-    }
+    public int getTaskId() { return taskId; }
+    public void setTaskId(int id) { this.taskId = id; }
     
-    public int getUsedTime() {
-        return UsedTime;
-    }
+    public int getCatId() { return catId; }
+    public void setCatId(int catId) { this.catId = catId; }
 
-    public void setUsedTime(int time) {
-        this.UsedTime = time;
-    }
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
+   
+    public int getUsedTime() { return usedTime; }
+    public void setUsedTime(int time) { this.usedTime = time; }
     
-    public String getComments() {
-        return comments;
-    }
-
-    public void setComments(String comments) {
-        this.comments = comments;
-    }
-    public ResultSet getTasks() throws SQLException, ClassNotFoundException {
+    public String getComments() { return comments; }
+    public void setComments(String comments) { this.comments = comments; }
+    
+    public CachedRowSet getTasks() { return tasks; }
+    public void setTasks(CachedRowSet tasks) { this.tasks = tasks; }  
+     
+    
+    public void forTech() throws SQLException, ClassNotFoundException {
         Connection cn = null;
-         
         try{
             //Laddar db-library (derby) till applikationens minne
             Class.forName("org.apache.derby.jdbc.ClientDriver");
@@ -89,73 +77,52 @@ public class TasksBean {
             cn = DriverManager.getConnection("jdbc:derby://localhost:1527/ps3;create=true;user=ps3;password=ps3");
             //specificerar vilken SQL-fråga som ska köras mot db
             PreparedStatement stmt =
-             cn.prepareStatement("SELECT ID, PROBLEM_ID, CATEGORY, ESTTIME, USEDTIME, PRIORITY, COMMENTS FROM TASK WHERE TECH_ID = ?");
+             cn.prepareStatement("SELECT * FROM TASK WHERE TECH_ID = ?");
             //Läser in en variabel som ersätter "?" i SQL-frågan 
             stmt.setInt(1, getTechId());
             //skapa en instans av det objekt som ska hålla resultatet av SQL-frågan
             tasks = new CachedRowSetImpl(); 
             //kör SQL-frågan "select..." och returnerar ett Resulset som läses in i result-objektet "CachedRowSet"
             tasks.populate(stmt.executeQuery()); 
-            return tasks;//returnerar posterna (resultset) från denna metod... 
-        }
-        /*catch (ClassNotFoundException | SQLException ex) {
-            System.out.println("Source: " + TasksBean.class.getName() + " Error: " +  ex.getMessage());
-            //Skriver följande typ av meddelande i output från GlassFish
-            //Info:   Källa: se.ltu.d0007n.TasksBean Error: java.net.ConnectException : Error connecting to server localhost on port 1526 with message Connection refused.
-        }*/
-        finally{
-            if (cn!=null) cn.close();
-        }
-    }
-    public String updateTask() throws SQLException, ClassNotFoundException {
-//        Läs in information om db-uppkoppling via connection-pool OBS används ej i denna lösning!
-//        InitialContext ctx = new InitialContext();
-//        ds = (DataSource) ctx.lookup("jdbc/students");
-        //deklarera objekt-ref som används för uppkoppling mot db
-        Connection cn = null;
-        boolean newStatus;
-        try{
-            //Laddar db-library (derby) till applikationens minne
-            Class.forName("org.apache.derby.jdbc.ClientDriver");
-            //specificerar uppgifter som behövs för att koppla upp sig mot databasen
-            cn = DriverManager.getConnection("jdbc:derby://localhost:1527/ps3;create=true;user=ps3;password=ps3");
-            //specificerar vilket SQL-uttryck som ska köras mot db           
-            if("ongoing".equals(getstatus())){
-                PreparedStatement stmt =
-                    cn.prepareStatement("UPDATE TASK SET USEDTIME=?, COMMENTS=? WHERE ID=?");
-                
-                stmt.setInt(1, getUsedTime());
-                stmt.setString(2, getComments());
-                stmt.setInt(3, getTaskId());
-
-                //Kör SQL-uttryck
-                stmt.executeUpdate();
-            }
-            else {
-                newStatus = Boolean.parseBoolean(getstatus());
-                            PreparedStatement stmt =
-                    cn.prepareStatement("UPDATE TASK SET USEDTIME=?, COMMENTS=?, STATUS=? WHERE ID=?");
-                
-                stmt.setInt(1, getUsedTime());
-                stmt.setString(2, getComments());
-                stmt.setBoolean(3, newStatus);
-                stmt.setInt(4, getTaskId());
-
-                //Kör SQL-uttryck
-                stmt.executeUpdate();
-            }
-            
-            // return "index"; //navigera till index.xhtml
-            return "success"; //navigering via faces-config.xml
         }
         catch (ClassNotFoundException | SQLException ex) {
             System.out.println("Source: " + TasksBean.class.getName() + " Error: " +  ex.getMessage());
             //Skriver följande typ av meddelande i output från GlassFish
             //Info:   Källa: se.ltu.d0007n.TasksBean Error: java.net.ConnectException : Error connecting to server localhost on port 1526 with message Connection refused.
-            return "failure";
         }
         finally{
             if (cn!=null) cn.close();
+        }
+    }
+    public void updateTask() throws SQLException {
+        Connection cn = null;
+        try{
+            //Laddar db-library (derby) till applikationens minne
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
+            //specificerar uppgifter som behövs för att koppla upp sig mot databasen
+            cn = DriverManager.getConnection("jdbc:derby://localhost:1527/ps3;create=true;user=ps3;password=ps3");            
+            //Kontrollera uppkoppling mot db
+            if (cn == null){
+                throw new SQLException("Couldn´t connect to database!");
+            }
+            else {
+                PreparedStatement stmt = cn.prepareStatement(
+                "UPDATE TASK SET USEDTIME=?, COMMENTS=?, STATUS=? WHERE ID=?");
+
+                stmt.setInt(1, getUsedTime());
+                stmt.setString(2, getComments());
+                stmt.setString(3, getStatus());
+                stmt.setInt(4, getTaskId());
+
+                //Kör SQL-uttryck
+                stmt.executeUpdate();
+            }
+        }
+        catch (ClassNotFoundException | SQLException ex) {
+            throw new SQLException("Problem with db:" + ex.getMessage());
+        }finally{
+            if (cn!=null) 
+                cn.close();
         }
     }
 }
